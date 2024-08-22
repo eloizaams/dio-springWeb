53e99ed7cd7d8d190aa3f186a372ae1acb801be0
eloizaams <eloizaams@yahoo.com.br>
2024-08-22 18:21:41 -0300

    feat: create project

---
+++ springWeb/src/main/java/eloiza/springWeb/SpringWebApplication.java
@@ -0,0 +1,13 @@
+package eloiza.springWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApplication.class, args);
+	}
+
+}
